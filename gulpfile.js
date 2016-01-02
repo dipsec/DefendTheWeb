5dcdc11715def2f849458f0cdcaa73ba716ac535
0x6C77 <0x6C77@gmail.com>
2016-01-02 17:25:46 +0000

    Moved Gulp tasks into folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,7 @@
 // Include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
-// Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-// Move CSS
-gulp.task('css', function() {
-    return gulp.src('src/css/*.css')
-        .pipe(gulp.dest('dist/css'));
-});
-
-// Compile our Sass
-gulp.task('sass', function() {
-    return gulp.src('src/css/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
-});
-
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(concat('script.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('src/js/*.js', ['lint', 'scripts']);
-    gulp.watch('src/css/*.scss', ['sass']);
-    gulp.watch('src/css/*.css', ['css']);
-});
+require('./gulp/main.js')();
 
 // Default Task
 gulp.task('default', ['lint', 'css', 'sass', 'scripts', 'watch']);
