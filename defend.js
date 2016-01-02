82f3a3380cd335ad94998c534b55a2f6dedc0bb6
0x6C77 <0x6C77@gmail.com>
2016-01-02 19:11:18 +0000

    Setup routes and layouts

--- defend.js
+++ defend.js
@@ -1,15 +1,30 @@
-var express = require('express');
-var app = express();
+var compression = require('compression'),
+	express = require('express'),
+	hbs = require('hbs'),
+	app = express();
+
+var site = require('./routes/site'),
+	post = require('./routes/post');
 
 const PORT = 8080;
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
-var server = app.listen(PORT, function () {
+// GZIP
+app.use(compression());
+
+// Serve static files
+app.use(express.static('static'));
+
+app.get('/', site.index);
+
+app.get('/posts', post.list);
+app.get('/post/:slug', post.view);
+
+var server = app.listen(PORT, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Defend the Web listening at http://%s:%s', host, port);
 });
---
+++ src/js/_splash.js
@@ -0,0 +1,65 @@
+$(function() {
+    particlesJS("network-background", {
+        "particles": {
+            "number": {
+                "value": 120,
+                "density": {
+                    "enable": true,
+                    "value_area": 1000
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "opacity": {
+                "value": 0.3,
+                "random": true
+            },
+            "size": {
+                "value": 3,
+                "random": true
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.1,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": true,
+                "straight": false,
+                "out_mode": "bounce"
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                    "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 0.2
+                    }
+                },
+                "push": {
+                    "particles_nb": 1
+                }
+            }
+        },
+        "retina_detect": true
+    });
+});
