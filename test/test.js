107205a04c0d54efa42e855c6b985113694b2622
0x6C77 <0x6C77@gmail.com>
2016-01-02 19:18:50 +0000

    Fixed test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Student', function () {
+    var student = { classes: [ 'English', 'Maths', 'Science' ] }
+
+    it('should have correct number of classes', function () {
+        assert.equal('Maths', student.classes[0]);
+    })
+})
