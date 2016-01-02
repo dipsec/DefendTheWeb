5e811ca268f496a9ad9ee08da0702a6c7c3106ab
0x6C77 <0x6C77@gmail.com>
2016-01-02 17:14:25 +0000

    Added seperate Gulp file for CI

---
+++ gulpfileCI.js
@@ -0,0 +1,40 @@
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Move CSS
+gulp.task('css', function() {
+    return gulp.src('src/css/*.css')
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Compile our Sass
+gulp.task('sass', function() {
+    return gulp.src('src/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Default Task
+gulp.task('default', ['lint', 'css', 'sass', 'scripts']);
